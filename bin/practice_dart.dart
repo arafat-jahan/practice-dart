6feab78414d35677760def2cd3b872b339a51658
arafat-jahan <ajh.official15@gmail.com>
2026-03-12 21:53:29 +0600

    function practice

--- bin/practice_dart.dart
+++ bin/practice_dart.dart
@@ -1,12 +1,15 @@
-void main() {
-  var candidates = ['Mahi','Roji','Trina','Mihi'];
-  var yearsExperience =[1,2,3,6];
 
-  for (int i = 0; i < candidates.length; i++) {
-    if (yearsExperience[i] < 5) {
-      continue;
-    }
+  int add( int n1 , int n2) => n1 + n2;
+  int sub(int n1, int n2) => n1 -n2;
+  int mul(int n1, int n2) => n1 * n2;
+  double div(int n1, int n2) => n1 / n2;
 
-    print("${candidates[i]} interview");
-  }
+  void main(){
+    int n1 = 30;
+    int n2 =50;
+    print("The sum is ${add(n1, n2)}");
+    print("The diff is ${sub(n1, n2)}");
+    print("The mul is ${mul(n1, n2)}");
+    print("The div is ${div(n1, n2)}");
+
 }
